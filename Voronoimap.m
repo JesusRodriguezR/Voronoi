ee3f7396e153629738623fc2a49fd46ee8a2456c
JesusRodriguezR <45362630+JesusRodriguezR@users.noreply.github.com>
2022-08-23 13:52:53 -0500

    Main file

---
+++ Voronoimap.m
@@ -0,0 +1,33 @@
+n = 256;
+voronoi = zeros(n,n,3);
+dvoronoi = double(voronoi);
+x=randi([1,n],2,16);
+answer = zeros(n,n,16);
+temp = 363;
+pnum = 1;
+color = randi ([0,256],1,16,3);
+
+for i = 1 : n
+    for j = 1 : n
+        
+        for p = 1 : 16
+        %dvoronoi(x(1,p),x(2,p),1) = 0; 
+        %temp = norm([i j]-[x(1,p) x(2,p)]);
+        if norm([i j]-[x(1,p) x(2,p)]) < temp 
+            temp = norm([i j]-[x(1,p) x(2,p)]);
+            pnum = p;
+        end
+
+
+        %answer(i,j,p) = norm([i j]-[x(1,p) x(2,p)]);
+        end
+        
+        dvoronoi(i,j,1) = color(1,pnum,1);
+        dvoronoi(i,j,2) = color(1,pnum,2);
+        dvoronoi(i,j,3) = color(1,pnum,3);
+        temp = 363;
+
+    end
+end
+result = uint8(dvoronoi);
+imshow(result)
