4810dd74430ef5ed1ba5e2ce1a49cac3b4f57501
JesusRodriguezR <45362630+JesusRodriguezR@users.noreply.github.com>
2022-08-23 19:34:17 -0500

    optimized code

--- Voronoimap.m
+++ Voronoimap.m
@@ -8,26 +8,16 @@ pnum = 1;
 color = randi ([0,256],1,16,3);
 
 for i = 1 : n
-    for j = 1 : n
-        
+    for j = 1 : n        
         for p = 1 : 16
-
         if norm([i j]-[x(1,p) x(2,p)]) < temp 
             temp = norm([i j]-[x(1,p) x(2,p)]);
             pnum = p;
         end
-
-
-       
-        end
-        
-        dvoronoi(i,j,1) = color(1,pnum,1);
-        dvoronoi(i,j,2) = color(1,pnum,2);
-        dvoronoi(i,j,3) = color(1,pnum,3);
+        end        
+        dvoronoi(i,j,:) = color(1,pnum,:);
         temp = 363;
-
     end
 end
 result = uint8(dvoronoi);
 imshow(result)
-
